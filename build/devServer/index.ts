2d9aa3705e45e71073a5429495a31dc929f76858
joey etamity <etamity@gmail.com>
2023-02-12 13:52:00 +0000

    update

--- build/devServer/index.ts
+++ build/devServer/index.ts
@@ -2,9 +2,9 @@ global.__dirname = `${process.cwd()}/dist`;
 global.__filename = `${process.cwd()}/dist/server.js`;
 
 import eventHandler from '../../server/lambda';
+//@ts-ignore
 import mockAlb from '../mockAlb';
 
-//@ts-ignore
 const mockAlbApp = mockAlb(eventHandler);
 
 export default mockAlbApp;
